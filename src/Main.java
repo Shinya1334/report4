a3bbe3f916784fc7a0b88c8d9a30cf42ade10bc4
shinya yoshitaka <ushimiyay20@gmail.com>
2018-11-27 02:43:04 +0900

    step3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args)
     {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }
-        catch (NullPointerException e)
-        {
-            System.out.println("strの値はnullです。");
-            System.out.println(e.getMessage());
-        }
+//        try {
+//            String str = null;
+//            System.out.println(str.length());
+//        }
+//        catch (NullPointerException e)
+//        {
+//            System.out.println("strの値はnullです。");
+//            System.out.println(e.getMessage());
+//        }
+
+        String str = "百二十三";
+        Integer.parseInt(str);
     }
 }
